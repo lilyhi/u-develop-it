a36311768ac1c0b15f256cdf4aec93d55bf93c8a
lilyhi <79773301+lilyhi@users.noreply.github.com>
2021-05-25 17:48:54 -0700

    added query method

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mysql = require('mysql2');
 const express = require('express');
 
 const PORT = process.env.PORT || 3001;
@@ -8,9 +9,22 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    // Your MySQL username,
+    user: 'root',
+    // Your MySQL password,
+    password: 'Lisasays7!',
+    database: 'election'
+  },
+  console.log('Connected to the election database.')
+);
 
-
-
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+  console.log(rows);
+});
 
 
 
