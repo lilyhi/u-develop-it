a18be650d589295835c2481591f8fa26bf12fa5f
lilyhi <79773301+lilyhi@users.noreply.github.com>
2021-05-27 22:05:33 -0700

    modularized the database connection logic

---
+++ db/connection.js
@@ -0,0 +1,29 @@
+const mysql = require('mysql2');
+
+// Connect to database
+const db = mysql.createConnection({
+    host: 'localhost',
+    // Your MySQL username,
+    user: 'root',
+    // Your MySQL password,
+    password: 'Lisasays7!',
+    database: 'election'
+  });
+  // console.log('Connected to the election database.')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = db;
